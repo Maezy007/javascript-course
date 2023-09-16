5137bc686b63d218bf51c52b19dd5f69656ac7c5
Mudassir Qureshi <mudassirqureshi789@gmail.com>
2023-09-16 21:02:12 +0500

    Lesson 11 (part2) completed Todo List and added seperate css file

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -6,8 +6,22 @@ function renderTodo() {
   let todoListHTML = '';
 
   for (let i = 0; i < todoList.length; i++) {
-    let todo = todoList[i]
-    const html = `<p>${todo}</p>`;
+    let todoObj = todoList[i];
+
+    // const name = todoObj.name;
+    // const dueDate = todoObj.dueDate;
+    const { name, dueDate } = todoObj;
+
+    const html =
+      `
+      <div>${name}</div>
+      <div>${dueDate}</div>
+
+        <button onclick="
+          todoList.splice(${i}, 1)
+          renderTodo();
+        ">Delete</button> 
+    `;
     todoListHTML += html;
   }
 
@@ -16,16 +30,29 @@ function renderTodo() {
 
 function addTodo() {
   let inputElement = document.querySelector('.js-input');
-
   let name = inputElement.value;
+
+  const dateInput = document.querySelector('.js-input1');
+  const dueDate = dateInput.value;
+  //   console.log('day:', typeof day === "number");
 
   // if (name === '') return alert('Please enter a task!');
 
-  todoList.push(name);
+  todoList.push({
+    //name: name,
+    //day: day
+
+    name,
+    dueDate
+  });
 
   console.log(todoList);
 
   inputElement.value = '';
+  dateInput.value = '';
+
+  // Show the todo list container once a todo is added
+  // document.querySelector('.js-todo-container').style.display = 'block';
 
   renderTodo();
 }
